d97fffd70e3fac0bf37d7625cb910f75fd282d29
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-09-26 02:20:29 +0000

    Add personType class

--- L10_A.cpp
+++ L10_A.cpp
@@ -4,3 +4,19 @@
     Purpose: Practice using nested structs
     Due date: 9/110/3025
 */
+
+#include <iostream>
+#include <iomanip>
+
+class personType {
+    public:
+        std::string first_name_;
+        std::string last_name_;
+        std::string address_;
+        double height_inches_{};
+        std::string date_of_birth_;  
+        char gender_{}; 
+
+        void print() const;
+        bool equals(const personType& other) const;
+};
